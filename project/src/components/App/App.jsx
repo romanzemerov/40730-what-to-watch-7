76417f735439fcdd2d2edf7a10cabe120f3c0c13
romanzemerov <romanzemerov@gmail.com>
2021-06-15 13:57:21 +0300

    Add app routes

--- project/src/components/App/App.jsx
+++ project/src/components/App/App.jsx
@@ -2,9 +2,43 @@ import React from 'react';
 import { Main } from '../pages/Main/Main';
 import PropTypes from 'prop-types';
 import * as types from '../../types';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AppRoutes } from '../../const';
+import { SignIn } from '../pages/SignIn/SignIn';
+import { WatchList } from '../pages/WatchList/WatchList';
+import { Movie } from '../pages/Movie/Movie';
+import { AddReview } from '../pages/AddReview/AddReview';
+import { Player } from '../pages/Player/Player';
+import { NotFound } from '../pages/NotFound/NotFound';
 
 function App({ movies }) {
-  return <Main movies={movies} />;
+  return (
+    <Router>
+      <Switch>
+        <Route path={AppRoutes.MAIN} exact>
+          <Main movies={movies} />
+        </Route>
+        <Route path={AppRoutes.SIGN_IN}>
+          <SignIn />
+        </Route>
+        <Route path={AppRoutes.WATCH_LIST}>
+          <WatchList />
+        </Route>
+        <Route path={AppRoutes.MOVIE} exact>
+          <Movie />
+        </Route>
+        <Route path={AppRoutes.ADD_REVIEW} exact>
+          <AddReview />
+        </Route>
+        <Route path={AppRoutes.PLAYER}>
+          <Player />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 App.propTypes = {
